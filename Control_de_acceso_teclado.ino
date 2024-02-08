02b25d3f8e833caf87c37914971dff6228a8b53e
Sergi Téllez Casals <120562205+Serteca@users.noreply.github.com>
2024-02-08 15:25:18 +0100

    Update Control_de_acceso_teclado.ino
    S'ha afegit una pantalla

--- Control_de_acceso_teclado.ino
+++ Control_de_acceso_teclado.ino
@@ -1,7 +1,21 @@
+#include <Key.h>
 #include <Keypad.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 #define ROW_NUM     4 // cuatro filas
 #define COLUMN_NUM  4 // cuatro columnas
+#define DHTPIN 27    //D27 del ESP27 Dev Module
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+//********* PANTALLA OLED
+#define oled 32
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, oled, &Wire);
 
 char keys[ROW_NUM][COLUMN_NUM] = {
   {'1', '2', '3', 'A'},
@@ -19,30 +33,57 @@ int storedNumber = 19921124; // Número previamente almacenado para comparación
 int userInput = 0; // Variable para almacenar el número introducido por el usuario
 int contador = 0;
 
-void setup() {
-  Serial.begin(9600); // Iniciar la comunicación con el puerto serie
-}
+ void setup() {
+   Serial.begin(9600);
+   dht.begin();
+   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
+   display.display();
+   display.clearDisplay();
+   display.display();
+   display.setTextSize(0.5);
+   display.setTextColor(WHITE,BLACK);
+ }
+
 
 void loop() {
   char key = keypad.getKey(); // Obtener la tecla presionada por el usuario
-  
+  display.setCursor(0, 0);
+
   if (key) {
     if (key >= '0' && key <= '9') {
+      display.println(key);
+      display.display();
       Serial.print("*"); // Imprimir la tecla presionada (se cambió println por print)
       userInput = userInput * 10 + (key - '0'); // Construir el número introducido por el usuario
       contador = contador + 1;
+      
     } 
     else {
-      Serial.println("\nLas letras y los símbolos no son válidos"); // Imprimir mensaje de tecla no válida
+      display.print("\nLas letras y los simbolos no son validos");
+      display.display();
+      delay(1000);
+      display.clearDisplay();
+      Serial.println("\nLas letras y los simbolos no son validos"); // Imprimir mensaje de tecla no válida
     }
   }
   
   if (contador == 8) {
     if (userInput == storedNumber) {
+      
+      display.print("\n¡Contrasenya correcta!");
+      display.display();
+      delay(1000);
+      display.clearDisplay();
       Serial.println("\n¡Contraseña correcta!"); // El número introducido es correcto
     } 
     else {
+      
+      display.print("\nContrasenya incorrecta, intentalo otra vez");
+      display.display();
+      
       Serial.println("\nContraseña incorrecta, intentalo otra vez"); // El número introducido es incorrecto
+      delay(1000);
+      display.clearDisplay();
     }
     userInput = 0; // Restablecer el valor de userInput para la próxima entrada
     contador = 0; // Reiniciar el contador
